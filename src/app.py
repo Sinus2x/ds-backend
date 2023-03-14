f84a93bb3f23a39ada7d2db43735c528c436e559
Maxim <hatin.max2002@gmail.com>
2023-03-14 19:29:36 +0000

    done all steps

--- src/app.py
+++ src/app.py
@@ -3,17 +3,65 @@ from flask import Flask, request
 from models.plate_reader import PlateReader, InvalidImage
 import logging
 import io
+import requests
 
 
 app = Flask(__name__)
 plate_reader = PlateReader.load_from_file('./model_weights/plate_reader_model.pth')
+IMAGES_URL = 'http://51.250.83.169:7878/images/'
 
+def process_response(im):
+    im = io.BytesIO(im)
+    try:
+        res = plate_reader.read_text(im)
+    except InvalidImage:
+        logging.error('invalid image')
+        return 'invalid image', 400
+
+    return {
+        'plate_number': res,
+    }
 
 @app.route('/')
 def hello():
     user = request.args['user']
     return f'<h1 style="color:red;"><center>Hello {user}!</center></h1>'
 
+# url: ?ids=["10022", "9965"]
+@app.route('/readNumbersByID')
+def get_numbers_by_id():
+    # if 'ids' not in request.args:
+    #     return 'field "ids" not found', 400
+    ids = request.form.getlist('ids')
+    res = []
+    for id in ids:
+        im = requests.get(
+            f'{IMAGES_URL}{id}'
+        )
+        if not im.ok:
+            return "image with this id doesn't exist or image-server error", 500
+        res.append(process_response(im.content))
+    return {'result': res}
+
+# url: ?id=10022
+@app.route('/readNumberByID')
+def get_number_by_id():
+    if 'id' not in request.json:
+        return 'field "id" not found', 400
+    id = request.json['id']
+    if not id.isnumeric():
+        return 'field "id" must be integer', 400
+    if not int(id) >= 0:
+        return '"id" must be non-negative', 400
+    
+    im = requests.get(
+        f'{IMAGES_URL}{id}'
+    )
+    if not im.ok:
+        return "image with this id doesn't exist or image-server error", 500
+    return process_response(im.content)
+    
+    
 
 # <url>:8080/greeting?user=me
 # <url>:8080 : body: {"user": "me"}
@@ -34,17 +82,7 @@ def greeting():
 @app.route('/readPlateNumber', methods=['POST'])
 def read_plate_number():
     im = request.get_data()
-    im = io.BytesIO(im)
-
-    try:
-        res = plate_reader.read_text(im)
-    except InvalidImage:
-        logging.error('invalid image')
-        return {'error': 'invalid image'}, 400
-
-    return {
-        'plate_number': res,
-    }
+    return process_response(im)
 
 
 if __name__ == '__main__':
--- src/plate_reader_client.py
+++ src/plate_reader_client.py
@@ -1,19 +1,42 @@
 import requests
+import json
+from typing import List
 
 
 class PlateReaderClient:
     def __init__(self, host: str):
         self.host = host
 
+    def _process_response(self, res):
+        if not res.ok:
+            return f'Error {res.status_code}: {res.text}'
+        return res.json()
+
     def read_plate_number(self, im):
         res = requests.post(
             f'{self.host}/readPlateNumber',
             headers={'Content-Type': 'application/x-www-form-urlencoded'},
-            data=im,
+            data=im
         )
+        return self._process_response(res)
 
-        return res.json()
+    def read_number_by_id(self, id: str):
+        res = requests.get(
+            f'{self.host}/readNumberByID',
+            json={
+                'id': id
+            }
+        )
+        return self._process_response(res)
 
+    def read_numbers_by_id(self, ids: List[str]):
+        res = requests.get(
+            f'{self.host}/readNumbersByID',
+            data={
+                'ids': ids
+            }
+        )
+        return self._process_response(res)
 
     def greeting(self, user: str):
         res = requests.post(
@@ -23,12 +46,15 @@ class PlateReaderClient:
                 'user': user,
             },
         )
-
-        return res.json()
+        return self._process_response(res)
 
 
 if __name__ == '__main__':
     client = PlateReaderClient(host='http://127.0.0.1:8080')
     with open('./images/9965.jpg', 'rb') as im:
         res = client.read_plate_number(im)
+        print(res)
+        res = client.read_number_by_id('10022')
+        print(res)
+        res = client.read_numbers_by_id(['10022', '9965'])
         print(res)
